b81fc21a49b4cc3303925324a19593ee2d44854e
bluefoxbandido <adamleeparkerjr@outlook.com>
2021-11-01 18:07:55 -0500

    error

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from waitress import serve
 
-from dotenv import load_dotenv
 from database.db import initialize_db
 from resources.routes import initialize_routes
 
 import os
 
-load_dotenv()
+
 
 app = Flask(__name__)
 api = Api(app)
